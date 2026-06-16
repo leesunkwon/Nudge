691aa89a30eb7316344e5d678160786cdb9cd403
스윗남 (Kotlinsun) <lsk859797@gmail.com>
2026-06-16 10:55:02 +0900

    프로젝트 초기 세팅

---
+++ Nudge/Nudge/NudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeApp.swift
+//  Nudge
+//
+//  Created by sunkwon on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct NudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
